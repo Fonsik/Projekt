becf201b83028f8dd1e24b74d4869afa4fa0bc4a
Fonsik <30571284+Fonsik@users.noreply.github.com>
2017-08-06 14:16:29 +0200

    Nowy początek

--- ver1.cpp
+++ ver1.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Statek {
+  char znak;
+ public:
+  int rozmiar;
+  int ruch;
+  int atak;
+  Statek (char, int, int, int) ;
+  virtual ~Statek() {};
+  int trafienie ;
+  void hit ();
+
+ Statek::Statek (char _c, int _r, int _x, int _a) : znak (_c), rozmiar (_r), ruch (_x), atak (_a) {}
+
+
+};
+
+int main ()
+{
+    Statek Motorowka ('M',2,2,0);
+    Statek Niszczyciel ('#',3,1,1);
+    Statek Torpedowiec ('%',4,0,2);
+    return 0;
+}
+
 
